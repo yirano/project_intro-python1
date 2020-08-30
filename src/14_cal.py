35d1d7e32952c4ad229e0202b3058b9b8018446d
yirano <yirano@protonmail.ch>
2020-08-30 17:53:15 -0400

    Complete 14_cal

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,12 +18,12 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
@@ -31,15 +31,20 @@ import sys
 import calendar
 from datetime import datetime
 
+try:
+    if len(sys.argv) == 2:
+        year = datetime.now().year
+        month = int(sys.argv[1])
 
-def render_calendar(y=2020, m=8):
+    else:
+        year = int(sys.argv[1])
+        month = int(sys.argv[2])
+except IndexError:
+    exit("Please provide a month, year, or both.")
+
+
+def render_calendar(y, m):
     return calendar.month(y, m)
 
-
-# testing = calendar.month(2014, 2)
-# print(testing)
-
-month = int(input('Month:  ') or "8")
-year = int(input('Year: ') or "2020")
 
 print(render_calendar(year, month))
