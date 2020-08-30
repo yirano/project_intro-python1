b999f6d6f261a2401f44fcd4f4f0e406e22ce15e
yirano <yirano@protonmail.ch>
2020-08-30 17:21:36 -0400

    Assume a mnth has been input when just ONE variable is given

--- src/16_cal2.py
+++ src/16_cal2.py
@@ -18,12 +18,12 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
@@ -31,14 +31,18 @@ import sys
 import calendar
 from datetime import datetime
 
+print(f'Length: {len(sys.argv)}')
+try:
+    if len(sys.argv) == 2:
+        year = 2020
+        month = int(sys.argv[1])
 
-if len(sys.argv) > 0:
-    try:
+    else:
         year = int(sys.argv[1])
         month = int(sys.argv[2])
-    except IndexError:
-        year = 2020
-        month = 8
+except IndexError:
+    year = 2020
+    month = 8
 
 
 def render_calendar(y, m):
